a6e408b94a7aaaf3cdcb1fe61f013ed2d1be0f42
williamgromstad <william.esbjug.gromstad@nmbu.no>
2024-11-08 11:50:00 +0100

    add file

---
+++ GIT.R
@@ -0,0 +1,33 @@
+library(usethis)
+usethis::use_git()
+use_github()
+
+git_default_branch_rename()
+git_default_branch_configure(name = "main")
+
+ 
+library(usethis)
+create_from_github("marabat/OpenScienceCourse", fork = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
